605aec76cd7b61106bca285fbcdac96ef2295ea2
tholor <malte.pietsch@deepset.ai>
2018-12-18 10:09:30 +0100

    fix cur_id count for on_memory. add documentation

--- examples/run_lm_finetuning.py
+++ examples/run_lm_finetuning.py
@@ -25,7 +25,7 @@ from tqdm import tqdm, trange
 
 import numpy as np
 import torch
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import DataLoader, RandomSampler
 from torch.utils.data.distributed import DistributedSampler
 
 from pytorch_pretrained_bert.tokenization import BertTokenizer
@@ -35,9 +35,9 @@ from pytorch_pretrained_bert.optimization import BertAdam
 from torch.utils.data import Dataset
 import random
 
-logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
-                    datefmt = '%m/%d/%Y %H:%M:%S',
-                    level = logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -47,16 +47,16 @@ class BERTDataset(Dataset):
         self.tokenizer = tokenizer
         self.seq_len = seq_len
         self.on_memory = on_memory
-        self.corpus_lines = corpus_lines #number of non-empty lines in input corpus
+        self.corpus_lines = corpus_lines  # number of non-empty lines in input corpus
         self.corpus_path = corpus_path
         self.encoding = encoding
-        self.current_doc = 0 #to avoid random sentence from same doc
+        self.current_doc = 0  # to avoid random sentence from same doc
 
-        #load samples directly from file
-        self.sample_counter = 0 #used to keep track of full epochs on file
-        self.line_buffer = None #keep second sentence of a pair in memory and use as first sentence in next pair
+        # for loading samples directly from file
+        self.sample_counter = 0  # used to keep track of full epochs on file
+        self.line_buffer = None  # keep second sentence of a pair in memory and use as first sentence in next pair
 
-        #load samples in memory
+        # for loading samples in memory
         self.current_random_doc = 0
         self.num_docs = 0
 
@@ -74,7 +74,7 @@ class BERTDataset(Dataset):
                     else:
                         doc.append(line)
                         self.corpus_lines =  self.corpus_lines + 1
-            #if last row in file is not empty
+            # if last row in file is not empty
             if self.all_docs[-1] != doc:
                 self.all_docs.append(doc)
 
@@ -98,29 +98,29 @@ class BERTDataset(Dataset):
             self.file = open(corpus_path, "r", encoding=encoding)
             self.random_file = open(corpus_path, "r", encoding=encoding)
 
-
     def __len__(self):
         # last line of doc won't be used, because there's no "nextSentence". Additionally, we start counting at 0.
         return self.corpus_lines - self.num_docs - 1
 
     def __getitem__(self, item):
-        #after one epoch we start again from beginning of file
         cur_id = self.sample_counter
         self.sample_counter += 1
-        if cur_id != 0 and (cur_id % (self.corpus_lines-1) == 0):
-            self.file.close()
-            self.file = open(self.corpus_path, "r", encoding=self.encoding)
+        if not self.on_memory:
+            # after one epoch we start again from beginning of file
+            if cur_id != 0 and (cur_id % len(self) == 0):
+                self.file.close()
+                self.file = open(self.corpus_path, "r", encoding=self.encoding)
 
         t1, t2, is_next_label = self.random_sent(item)
 
-        #tokenize
+        # tokenize
         tokens_a = self.tokenizer.tokenize(t1)
         tokens_b = self.tokenizer.tokenize(t2)
 
-        #combine to one sample
+        # combine to one sample
         cur_example = InputExample(guid=cur_id, tokens_a=tokens_a, tokens_b=tokens_b, is_next=is_next_label)
 
-        #transform sample to features
+        # transform sample to features
         cur_features = convert_example_to_features(cur_example, self.seq_len, self.tokenizer)
 
         cur_tensors = {"input_ids": torch.tensor(cur_features.input_ids),
@@ -131,8 +131,13 @@ class BERTDataset(Dataset):
 
         return cur_tensors
 
-
     def random_sent(self, index):
+        """
+        Get one sample from corpus consisting of two sentences. With prob. 50% these are two subsequent sentences
+        from one doc. With 50% the second sentence will be a random one from another doc.
+        :param index: int, index of sample.
+        :return: (str, str, int), sentence 1, sentence 2, isNextSentence Label
+        """
         t1, t2 = self.get_corpus_line(index)
         if random.random() > 0.5:
             label = 0
@@ -145,11 +150,16 @@ class BERTDataset(Dataset):
         return t1, t2, label
 
     def get_corpus_line(self, item):
+        """
+        Get one sample from corpus consisting of a pair of two subsequent lines from the same doc.
+        :param item: int, index of sample.
+        :return: (str, str), two subsequent sentences from corpus
+        """
         t1 = ""
         t2 = ""
         assert item < self.corpus_lines
         if self.on_memory:
-            #get the right doc
+            # get the right doc
             doc_id = 0
             doc_start = 0
             doc_end = len(self.all_docs[doc_id]) - 2
@@ -157,24 +167,24 @@ class BERTDataset(Dataset):
                 doc_id += 1
                 doc_start = doc_end + 1
                 doc_end += len(self.all_docs[doc_id]) - 1
-            #get the right line within doc
+            # get the right line within doc
             line_in_doc = item - doc_start
             t1 = self.all_docs[doc_id][line_in_doc]
             t2 = self.all_docs[doc_id][line_in_doc + 1]
-            #used later to avoid random nextSentence from same doc
+            # used later to avoid random nextSentence from same doc
             self.current_doc = doc_id
             return t1, t2
         else:
             if self.line_buffer is None:
-                #read first non-empty line of file
+                # read first non-empty line of file
                 while t1 == "" :
                     t1 = self.file.__next__().strip()
                     t2 = self.file.__next__().strip()
             else:
-                #use t2 from previous iteration as new t1
+                # use t2 from previous iteration as new t1
                 t1 = self.line_buffer
                 t2 = self.file.__next__().strip()
-                #skip empty rows that are used for separating documents and keep track of current doc id
+                # skip empty rows that are used for separating documents and keep track of current doc id
                 while t2 == "" or t1 == "":
                     t1 = self.file.__next__().strip()
                     t2 = self.file.__next__().strip()
@@ -241,8 +251,8 @@ class InputExample(object):
         self.guid = guid
         self.tokens_a = tokens_a
         self.tokens_b = tokens_b
-        self.is_next = is_next #nextSentence
-        self.lm_labels = lm_labels #masked words for language model
+        self.is_next = is_next  # nextSentence
+        self.lm_labels = lm_labels  # masked words for language model
 
 
 class InputFeatures(object):
@@ -257,11 +267,17 @@ class InputFeatures(object):
 
 
 def random_word(tokens, tokenizer):
+    """
+    Masking some random tokens for Language Model task with probabilities as in the original BERT paper.
+    :param tokens: list of str, tokenized sentence.
+    :param tokenizer: Tokenizer, object used for tokenization (we need it's vocab here)
+    :return: (list of str, list of int), masked tokens and related labels for LM prediction
+    """
     output_label = []
 
     for i, token in enumerate(tokens):
         prob = random.random()
-        #mask token with 15% probability
+        # mask token with 15% probability
         if prob < 0.15:
             prob /= 0.15
 
@@ -273,23 +289,30 @@ def random_word(tokens, tokenizer):
             elif prob < 0.9:
                 tokens[i] = random.choice(list(tokenizer.vocab.items()))[0]
 
-            #-> rest 10% randomly keep current token
+            # -> rest 10% randomly keep current token
 
-            #append current token to output (we will predict these later)
+            # append current token to output (we will predict these later)
             try:
                 output_label.append(tokenizer.vocab[token])
             except KeyError:
-                #For unknown words (should not occur with BPE vocab)
+                # For unknown words (should not occur with BPE vocab)
                 output_label.append(tokenizer.vocab["[UNK]"])
         else:
-            #no masking token (will be ignored by loss function later)
+            # no masking token (will be ignored by loss function later)
             output_label.append(-1)
 
     return tokens, output_label
 
 
 def convert_example_to_features(example, max_seq_length, tokenizer):
-    """Loads a data file into a list of `InputBatch`s."""
+    """
+    Convert a raw sample (pair of sentences as tokenized strings) into a proper training sample with
+    IDs, LM labels, input_mask, CLS and SEP tokens etc.
+    :param example: InputExample, containing sentence input as strings and is_next label
+    :param max_seq_length: int, maximum length of sequence.
+    :param tokenizer: Tokenizer
+    :return: InputFeatures, containing all inputs and labels of one sample as IDs (as used for model training)
+    """
     tokens_a = example.tokens_a
     tokens_b = example.tokens_b
     # Modifies `tokens_a` and `tokens_b` in place so that the total
@@ -299,7 +322,7 @@ def convert_example_to_features(example, max_seq_length, tokenizer):
 
     t1_random, t1_label = random_word(tokens_a, tokenizer)
     t2_random, t2_label = random_word(tokens_b, tokenizer)
-    #concatenate lm labels and account for CLS, SEP, SEP
+    # concatenate lm labels and account for CLS, SEP, SEP
     lm_label_ids = ([-1] + t1_label + [-1] + t2_label + [-1])
 
     # The convention in BERT is:
@@ -371,7 +394,7 @@ def convert_example_to_features(example, max_seq_length, tokenizer):
                              input_mask=input_mask,
                              segment_ids=segment_ids,
                              lm_label_ids=lm_label_ids,
-                             is_next = example.is_next)
+                             is_next=example.is_next)
     return features
 
 
@@ -540,12 +563,12 @@ def main():
         logger.info("  Num steps = %d", num_train_steps)
 
         if args.local_rank == -1:
-            #train_sampler = RandomSampler(train_dataset)
-            train_sampler = SequentialSampler(train_dataset)
+            train_sampler = RandomSampler(train_dataset)
         else:
-            #TODO: check if this works with our data generator that relies on file.__next__ (doesn't return item back by index)
+            #TODO: check if this works with current data generator from disk that relies on file.__next__
+            # (it doesn't return item back by index)
             train_sampler = DistributedSampler(train_dataset)
-        train_dataloader = DataLoader(train_dataset, sampler= train_sampler, batch_size=args.train_batch_size)
+        train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
 
         model.train()
         for _ in trange(int(args.num_train_epochs), desc="Epoch"):
@@ -610,9 +633,11 @@ def _truncate_seq_pair(tokens_a, tokens_b, max_length):
         else:
             tokens_b.pop()
 
+
 def accuracy(out, labels):
     outputs = np.argmax(out, axis=1)
     return np.sum(outputs == labels)
+
 
 def copy_optimizer_params_to_model(named_params_model, named_params_optimizer):
     """ Utility function for optimize_on_cpu and 16-bits training.
@@ -623,6 +648,7 @@ def copy_optimizer_params_to_model(named_params_model, named_params_optimizer):
             logger.error("name_opti != name_model: {} {}".format(name_opti, name_model))
             raise ValueError
         param_model.data.copy_(param_opti.data)
+
 
 def set_optimizer_params_grad(named_params_optimizer, named_params_model, test_nan=False):
     """ Utility function for optimize_on_cpu and 16-bits training.
@@ -644,21 +670,5 @@ def set_optimizer_params_grad(named_params_optimizer, named_params_model, test_n
     return is_nan
 
 
-def test():
-    data_dir = "samples/small_corpus.txt"
-    tokenizer = BertTokenizer.from_pretrained("/home/mp/deepset/dev/bert/models/base-cased/vocab.txt")
-    #tokenizer = BertTokenizer.from_pretrained(bert_model_name)
-
-    max_seq_length = 32
-    train_dataset = BERTDataset(data_dir, tokenizer, seq_len=max_seq_length,
-                                corpus_lines=None, on_memory=False)
-
-    #train_sampler = RandomSampler(train_dataset)
-
-    train_dataloader = DataLoader(train_dataset, batch_size=12)
-    for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
-        a = 1
-
 if __name__ == "__main__":
-    #test()
     main()
